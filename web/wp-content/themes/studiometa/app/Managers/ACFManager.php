2446c8dfc161c5b831cd447e0e0b0c150d491f1b
Guillaume <guillaume@studiometa.fr>
2022-09-22 10:52:43 +0200

    Create pagebuilder with default blocks

--- web/wp-content/themes/studiometa/app/Managers/ACFManager.php
+++ web/wp-content/themes/studiometa/app/Managers/ACFManager.php
@@ -12,6 +12,11 @@ namespace Studiometa\Managers;
 
 use Studiometa\WPToolkit\Managers\ManagerInterface;
 use StoutLogic\AcfBuilder\FieldsBuilder;
+use Studiometa\Blocks\Accordion;
+use Studiometa\Blocks\ImageFullWidth;
+use Studiometa\Blocks\PushImageText;
+use Studiometa\Blocks\Video;
+use Studiometa\Blocks\Wysiwyg;
 
 /** Class **/
 class ACFManager implements ManagerInterface {
@@ -31,83 +36,11 @@ class ACFManager implements ManagerInterface {
 	public function register_acf_builder_group() {
 		$builder_group = new FieldsBuilder( 'builder_group', array( 'title' => 'Page builder' ) );
 		$builder_group->addFlexibleContent( 'builder', array( 'button_label' => 'Ajouter section' ) )
-			->addLayout( 'block_push_img_txt', array( 'label' => 'Bloc Image + Texte' ) )
-				->addImage( 'image', array( 'label' => 'Image' ) )
-				->addWysiwyg( 'content', array( 'label' => 'Contenu' ) )
-				->addLink(
-					'cta',
-					array(
-						'label'             => 'Bouton CTA',
-						'instructions'      => 'Ajoute un CTA, si non renseigné il ne sera pas visible',
-						'conditional_logic' => 0,
-						'return_format'     => 'array',
-					)
-				)
-				->addRadio(
-					'text_position',
-					array(
-						'label'         => 'Position du bloc texte',
-						'choices'       =>
-						array(
-							'left'  => 'Gauche',
-							'right' => 'Droite',
-						),
-						'default_value' => 'left',
-					)
-				)
-			->addLayout( 'block_wysiwyg', array( 'label' => 'Bloc Texte simple' ) )
-				->addWysiwyg(
-					'content',
-					array(
-						'label'   => 'Contenu',
-						'toolbar' => 'full',
-					)
-				)
-			->addLayout( 'block_video', array( 'label' => 'Bloc vidéo' ) )
-				->addText( 'title', array( 'label' => 'Titre de la vidéo' ) )
-				->addFile(
-					'video',
-					array(
-						'label'        => 'Vidéo',
-						'instructions' => 'Format MP4 à privilégier',
-					)
-				)
-				->addImage(
-					'video_cover',
-					array(
-						'label'        => 'Vidéo cover',
-						'instructions' => 'Image de couverture',
-					)
-				)
-				->addText(
-					'video_legend',
-					array(
-						'label'        => 'Légende vidéo',
-						'instructions' => 'Caché si non renseigné',
-					)
-				)
-			->addLayout( 'block_image_full_width', array( 'label' => 'Bloc image pleine largeur' ) )
-				->addImage(
-					'image',
-					array(
-						'label'        => 'Image',
-						'instructions' => 'Format HD à privilégier',
-					)
-				)
-			->addLayout( 'block_accordeon', array( 'label' => 'Bloc Accordeon' ) )
-				->addRepeater(
-					'theme',
-					array(
-						'label'  => 'Chapitre',
-						'layout' => 'block',
-					)
-				)
-					->addText( 'theme_title', array( 'label' => 'Titre du chapitre' ) )
-					->addRepeater( 'list' )
-						->addText( 'list_title', array( 'label' => 'Titre' ) )
-						->addWysiwyg( 'list_content', array( 'label' => 'Contenu' ) )
-					->endRepeater()
-				->endRepeater()
+			->addLayout(Video::get_block())
+			->addLayout(PushImageText::get_block())
+			->addLayout(Wysiwyg::get_block())
+			->addLayout(ImageFullWidth::get_block())
+			->addLayout(Accordion::get_block())
 			->setLocation( 'post_type', '==', 'page' );
 			acf_add_local_field_group( $builder_group->build() );
 	}
